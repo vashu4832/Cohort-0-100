793a618236692c7e76b920636ced085804d995d4
ashu <ashutoshvishwakarma222@gmail.com>
2026-02-26 16:58:55 +0530

    demonstrating why do we need filter function

--- demo.js
+++ demo.js
@@ -8,3 +8,16 @@ function transform(i){
 
 let ans = input.map(transform);
 console.log(ans);
+
+// What if I tell you, given an input array give me back all the even values from it
+
+const arr = [1,2,3,4,5];
+
+const newArr = [];
+for(let i=0; i<arr.length; i++){
+    if(arr[i] % 2 == 0){
+        newArr.push(arr[i]);
+    }
+}
+
+console.log(newArr);
