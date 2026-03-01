e549f736223b2fb16306fa45ccb4423e73570510
ashu <ashutoshvishwakarma222@gmail.com>
2026-03-01 18:06:11 +0530

    using middlewares to avoid DRY principle

--- middlewares/new.js
+++ middlewares/new.js
@@ -2,24 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/health-checkup", (req,res) => {
-    const kidneyId = req.query.kidneyId;
+function userMiddleware(req, res, next){
     const username = req.headers.username;
     const password = req.headers.password;
-
     if(username != "Ashutosh" && password != "pass"){
         res.status(403).json({
             msg: "User doesn't exists"
         })
-        return;
+    } else {
+        next();
     }
+}
 
-    if(kidneyId != 1 && kidneyId !=2){
-        res.status(411).json({
-            msg: "Wrong input"
-        });
-        return;
+function kidneyMiddleware(req, res, next){
+    const kidneyId = req.query.kidneyId;
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(403).json({
+            msg: "User doesn't exists"
+        })
+    } else {
+        next();
     }
+}
+
+app.get("/health-checkup", userMiddleware, kidneyMiddleware, (req,res) => {
+   
+    
 
     res.send("Your heart is healthy");
 
