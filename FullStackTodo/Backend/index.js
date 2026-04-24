c5e60928ab4dc305cded8d08e07bd3da5a82bb3b
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-24 10:18:25 +0530

    added validations

--- FullStackTodo/Backend/index.js
+++ FullStackTodo/Backend/index.js
@@ -1,20 +1,37 @@
 // Write basic express boilerplate code with express.json middleware
 const express = require("express");
 const app = express();
-const {createTodo, updateTodo} = require("./types"); 
+const { createTodo, updateTodo } = require("./types");
 
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-     
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong input"
+        })
+        return;
+    }
+
+    // put it in mongoDb
+
 })
 
-app.get("/todos", (req,res) => {
+app.get("/todos", (req, res) => {
 
 })
 
 app.put("/completed", (req, res) => {
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload );
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You send the wrong id."
+        })
+        return;  
+    }
 })
 
 app.listen(3000, () => {
