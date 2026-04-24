adad5b98af388ce35329817e36dff298c24426fe
ashu <ashutoshvishwakarma222@gmail.com>
2026-04-24 12:55:56 +0530

    final completed

--- FullStackTodo/Frontend/src/App.jsx
+++ FullStackTodo/Frontend/src/App.jsx
@@ -1,13 +1,20 @@
+import { useState, useEffect } from "react"
 import { CreateTodo } from "./components/CreateTodo"
 import { Todos } from "./components/Todos"
 
 function App() {
-  
+  const [todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todos")
+  .then(async(res) => {
+    const json = await res.json();
+    setTodos(json.todos);
+  })
 
   return (
     <>
       <CreateTodo/>
-      <Todos/>
+      <Todos todos={todos}/>
     </>
   )
 }
--- FullStackTodo/Frontend/src/components/CreateTodo.jsx
+++ FullStackTodo/Frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export function CreateTodo(){
-    return(
-        <div>
-            <input type='text' placeholder='title' /><br/><br/>
-            <input type='text' placeholder='description' /><br/><br/>
-            <button>Add Todo</button>
-        </div>
-    )
+export function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div>
+      <input
+        id="title"
+        type="text"
+        placeholder="title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <br />
+      <br />
+      <input
+        id="desc"
+        type="text"
+        placeholder="description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: description,
+            }),
+            headers: {
+              "Content-type": "application/json",
+            },
+          }).then(async (res) => {
+            const json = await res.json();
+            alert("Todo added");
+          });
+        }}
+      >
+        Add Todo
+      </button>
+    </div>
+  );
 }
