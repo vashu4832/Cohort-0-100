dceca9a4a3cce79720b93282e7cf607e63ed2351
ashu <ashutoshvishwakarma222@gmail.com>
2026-05-06 09:51:15 +0530

    added CountContext in MyContext

--- RoutesBasics/src/App.jsx
+++ RoutesBasics/src/App.jsx
@@ -1,34 +1,59 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { CountContext } from "./Context";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  // wrap anyone that want to use the teleported value inside a provider
   return (
     <div>
-      <Count count={count} setCount={setCount}/>
-      
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
   );
 }
 
-function Count({count, setCount}){
-  return <div>
-    <CountRenderer count={count} />
-    <br/>
-    <Buttons setCount={setCount} count={count}/>
-  </div>
+function Count({ setCount }) {
+  return (
+    <div>
+      <CountRenderer />
+      <br />
+      <Buttons setCount={setCount} />
+    </div>
+  );
 }
 
-function CountRenderer({count}){
-  return <div>
-    <h3>{count}</h3>
-  </div>
+function CountRenderer() {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <h3>{count}</h3>
+    </div>
+  );
 }
 
-function Buttons({setCount, count}){
-  return <div>
-    <button onClick={() => {setCount(count+1)}}>Increase</button>
-    <button onClick={() => {setCount(count-1)}}>Decrease</button>
-  </div>
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
 }
 
 export default App;
